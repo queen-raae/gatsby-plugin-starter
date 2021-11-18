0d2f4c44ee916cc89eb2cb6a5906d9a72a7353a4
Benedicte Raae <raae@raae.codes>
2021-11-18 10:27:39 +0100

    Move to yarn

--- plugin/gatsby-node.js
+++ plugin/gatsby-node.js
@@ -1,1 +1,10 @@
 // https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
+
+exports.pluginOptionsSchema = ({ Joi }) => {
+  return Joi.object({
+    option1: Joi.string()
+      .valid("unicorn", "pirate")
+      .default("unicorn")
+      .description(`Plugin option 1`),
+  });
+};
