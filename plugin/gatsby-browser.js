1378f40846a3202f8a47b7fb1510dff305c74973
Benedicte Raae <raae@raae.codes>
2022-02-03 13:22:53 +0100

    feat: better default plugin

--- plugin/gatsby-browser.js
+++ plugin/gatsby-browser.js
@@ -1,1 +1,10 @@
 // https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/
+
+const { EMOJIS } = require("./constants");
+
+exports.onInitialClientRender = (_, pluginOptions) => {
+  const { message, emoji } = pluginOptions;
+  const selectedEmoji = EMOJIS[emoji];
+
+  alert(`${selectedEmoji} ${message} ${selectedEmoji}`);
+};
