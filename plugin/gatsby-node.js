4e2a87552bf8fc684c801fe146d9b9dc5372ab2a
Benedicte Raae <raae@raae.codes>
2022-02-10 20:36:52 +0100

    feat: add plugin options schema tests

--- plugin/gatsby-node.js
+++ plugin/gatsby-node.js
@@ -6,7 +6,7 @@ const validKeys = Object.keys(EMOJIS);
 
 exports.pluginOptionsSchema = ({ Joi }) => {
   return Joi.object({
-    message: Joi.string().default("Hello from the Plugin"),
+    message: Joi.string().default("Hi from the plugin"),
     emoji: Joi.string()
       .valid(...validKeys)
       .default(validKeys[0])
